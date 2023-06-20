3f350e5805baa06495edb3827e89b49b20b98003
KashimaSho <s-kashima-9c2@eagle.sophia.ac.jp>
2023-06-20 17:54:45 +0900

    add argmentations.py

--- RandomBrightness.py
+++
@@ -1,6 +0,0 @@
-import random
-def __call__(self, image, boxes=None, labels=None):
-    if random.randint(2):
-        delta = random.uniform(-self.delta, self.delta)
-        image += delta
-    return image, boxes, labels
